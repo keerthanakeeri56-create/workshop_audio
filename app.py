960726fda9ddca90b3450f0bd6d57096c1f459f6
keerthanakeeri56-create <keerthanakeeri56@gmail.com>
2026-05-08 15:08:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
 
 def main():
     st.image("WhatsApp Image 2026-04-25 at 11.02.18 PM.jpeg")
-    st.title(" PragyanAI - VVIET Workshop: Audio Hub")
+    st.title(" 123: Audio Hub")
     
     audio_bytes = audio_recorder(text="Click to record", neutral_color="#6aa36f")
     
@@ -28,11 +28,11 @@ def main():
                 with sr.AudioFile(BytesIO(audio_bytes)) as source:
                     audio_data = recognizer.record(source)
                     text = recognizer.recognize_google(audio_data)
-                    st.success(f"**Original:** {text}")
+                    st.success(f"*Original:* {text}")
 
                 # 2. Translation (Using deep-translator)
                 translated_text = GoogleTranslator(source='auto', target=target_code).translate(text)
-                st.info(f"**Translated:** {translated_text}")
+                st.info(f"*Translated:* {translated_text}")
 
                 # 3. Text to Speech
                 tts = gTTS(text=translated_text, lang=target_code)
@@ -43,5 +43,5 @@ def main():
             except Exception as e:
                 st.error(f"Error: {e}")
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
